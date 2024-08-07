a03efee8624b6b3811da515f792e6e146ed6e774
Cardoso2626 <sueleidesaraiva@hotmail.com>
2024-08-06 22:18:28 -0300

    Testando diferentes tipos de log

---
+++ js/sript.js
@@ -0,0 +1,10 @@
+let frutas = ["Maça", "Banana", "Laranja", "Manga", "Açai", "Goiaba", "Ameixa",
+     "Cereja", "Mirtilo", "Uva"]
+
+//imprimir array com console.log e console.table
+
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[0])
+//=======================================LOOOPS FOR/ FOR OF/ FOR IN/ FOR EACH
+
