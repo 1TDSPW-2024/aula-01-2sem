9aa18d0e35364f0d2abee4bf19bed209e85da2d1
Cardoso2626 <sueleidesaraiva@hotmail.com>
2024-08-07 09:58:55 -0300

    método de renderizar lista

--- js/sript.js
+++ js/sript.js
@@ -8,3 +8,30 @@ console.table(frutas);
 console.log(frutas[0])
 //=======================================LOOOPS FOR/ FOR OF/ FOR IN/ FOR EACH
 
+//recuperando a lista ul que esta no html com id = lista
+let lista = document.getElementById("lista");
+function renderizaLista(){
+    lista.innerHTML = "";
+    for (let fruta of frutas){
+
+
+        let item = document.createElement("li");
+        item.textContent = fruta;
+    
+        lista.appendChild(item);
+    
+    }
+}
+
+
+//recuperando o botão de add
+
+document.querySelector("#btnAdd").addEventListener("click", ()=> {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.push(valorDigitadoNoCampo)
+
+    renderizaLista()
+
+})
+
+renderizaLista()
